40870a0f9be29b2c7b28c7a9fda545dfc47727fc
Kamran Muzaffarli <muzaffarli.kamran@gmail.com>
2026-06-22 20:06:00 +0400

    Organize SQL files

---
+++ database/database_checks.sql
@@ -0,0 +1,20 @@
+SELECT table_name
+FROM information_schema.tables
+WHERE table_schema = 'public'
+ORDER BY table_name;
+
+SELECT
+    tc.table_name,
+    kcu.column_name,
+    ccu.table_name AS foreign_table_name,
+    ccu.column_name AS foreign_column_name
+FROM information_schema.table_constraints AS tc
+JOIN information_schema.key_column_usage AS kcu
+    ON tc.constraint_name = kcu.constraint_name
+JOIN information_schema.constraint_column_usage AS ccu
+    ON ccu.constraint_name = tc.constraint_name
+WHERE tc.constraint_type = 'FOREIGN KEY'
+ORDER BY tc.table_name, kcu.column_name;
+
+SELECT *
+FROM material_attributes;
